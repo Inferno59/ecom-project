f1fd8026ba77ed04c4409ae0dd6065bde14baab4
Bender <Bender Test>
2020-10-10 15:24:56 +0500

    aff postgreesql DB

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecom',
+        'USER': 'webuser',
+        'PASSWORD': 'inferno59',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
